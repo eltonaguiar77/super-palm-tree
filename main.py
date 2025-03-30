6b1609a52e82a2a05e98b6310d0c8b8565b6401b
eltonaguiar77 <129380874+eltonaguiar77@users.noreply.github.com>
2025-03-30 17:56:42 -0300

    Criação Actions

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import random
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo: bool
 
 #127.0.00.1:8000/
 @app.get("/")
@@ -13,3 +19,16 @@ async def root():
 @app.get("/teste")
 async def funcaoteste():
     return {"teste": True, "Número aleatório": random.randint(0, 20000)}
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
+
