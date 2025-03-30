37e77187ddd968bdf651c6d9b8769fe280e36481
eltonaguiar77 <129380874+eltonaguiar77@users.noreply.github.com>
2025-03-29 21:36:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+#127.0.00.1:8000/
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
+#127.0.00.1:8000/teste1
 @app.get("/teste1")
 async def funcaoteste():
     return {"teste": True, "Número aleatório": random.randint(0, 1000)}
