95236d138e365bd6a32457cb6bbcfe85c781fe31
eltonaguiar77 <129380874+eltonaguiar77@users.noreply.github.com>
2025-04-18 18:25:34 -0300

    update src test

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ async def create_estudante(estudante: Estudante):
     return estudante
 
 @app.put("/estudantes/update/{id_estudante}")
-async def update_item(id_estudante: int):
+async def update_estudante(id_estudante: int):
     return id_estudante > 0
 
 @app.delete("/estudantes/delete/{id_estudante}")
